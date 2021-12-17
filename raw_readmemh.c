3151b61b883d81c623608c91ff2e6f83d14f3575
fumi <fumi01200924@gmail.com>
2021-12-17 23:56:49 +0900

    動いた

--- raw_readmemh.c
+++ raw_readmemh.c
@@ -6,20 +6,29 @@
 
 int main(void){
     FILE *fp = NULL;
+    FILE *outfp = NULL;
     unsigned char mem[HEIGHT * WIDTH * DEPTH];
 
     fp = fopen("kao_9696.raw", "rb");
-    if(fp==NULL){
+    outfp = fopen("testbin_image.txt", "w");
+
+    if(fp==NULL||outfp==NULL){
         printf("no file.\n");
         return -1;
     }
 
     fread(mem, sizeof(unsigned char), sizeof(mem)/sizeof(mem[0]), fp);
-    for(int i=0; i<10; i++){
-        printf("%02x ",mem[i]);
+    for(int i=0; i<HEIGHT*WIDTH; i++){
+        for(int j=0; j<DEPTH; j++){
+            fprintf(outfp, "%02x", mem[j+i*DEPTH]);
+            printf("%02x ", mem[j+i*DEPTH]);
+        }
+        fprintf(outfp, "\n");
+        printf("\n");
     }
+    printf("\ndone.\n");
 
     fclose(fp);
-    
+    fclose(outfp);
     return 0;
 }
