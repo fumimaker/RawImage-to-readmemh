ae4b3e62123012f434403e13f0c67f405fca310a
fumi <fumi01200924@gmail.com>
2021-12-17 23:40:44 +0900

    test OK

--- raw_readmemh.c
+++ raw_readmemh.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+#define HEIGHT  96
+#define WIDTH   96
+#define DEPTH   3
+
 int main(void){
-    printf("hello world.\n");
+    FILE *fp = NULL;
+    unsigned char mem[HEIGHT * WIDTH * DEPTH];
+
+    fp = fopen("kao_9696.raw", "rb");
+    if(fp==NULL){
+        printf("no file.\n");
+        return -1;
+    }
+
+    fread(mem, sizeof(unsigned char), sizeof(mem)/sizeof(mem[0]), fp);
+    for(int i=0; i<10; i++){
+        printf("%02x ",mem[i]);
+    }
+
+    fclose(fp);
+    
     return 0;
 }
