4893d09213c297c2490c5d461675596143ade06b
fumi <fumi01200924@gmail.com>
2021-12-18 03:05:58 +0900

    ugokanai

--- raw_macroblock.c
+++ raw_macroblock.c
@@ -10,12 +10,12 @@ int main(void){
     FILE *fp = NULL;
     FILE *outfp = NULL;
 //unsigned char mem[HEIGHT * WIDTH * DEPTH];
-    unsigned char mem[HEIGHT * WIDTH];
+    unsigned char mem[HEIGHT * WIDTH * DEPTH];
 
     fp = fopen(inputname, "rb");
     outfp = fopen(outputname, "w");
 
-    if(fp==NULL||outfp==NULL){
+    if (fp == NULL || outfp == NULL){
         printf("no file.\n");
         return -1;
     }
@@ -25,19 +25,23 @@ int main(void){
         //マクロブロックの中身
     for(int block_y=0; block_y<HEIGHT/8; block_y++){
         for(int block_x=0; block_x<WIDTH/8; block_x++){
-
             printf("%d block_x, %d block_y\n",block_x, block_y);
             for(int y=0; y<8; y++){
                 for(int x=0; x<8; x++){
-                    int addr = (x+block_x*8) + (y*WIDTH + HEIGHT*8*block_y);
-                    //fprintf(outfp, "%02x", mem[addr]);
-                    printf("%02x ", mem[addr]);
+                    int addr = 
+                    ((x+block_x*8) + (y*WIDTH + HEIGHT*8*block_y)) * DEPTH;
+
+                    for(int i=0; i<DEPTH; i++){
+                        fprintf(outfp, "%02x", mem[addr+i]);
+                    }
+                    fprintf(outfp, "\n");
+                    //printf("%02x ", mem[addr]);
                 }
                 printf("\n");
             }
+            printf("\n");
         }
-        
-        printf("\n");printf("\n");
+        printf("\n");
     }
     printf("\ndone.\n");
 
--- renzoku.c
+++ renzoku.c
@@ -3,9 +3,11 @@
 int main(){
     FILE *outfp = NULL;
     outfp = fopen("renzoku.txt", "wb");
-    unsigned char data[256];
+    unsigned char data[256*3];
     for(int i=0; i<256; i++){
-        data[i] = i;
+        for(int j=0; j<3; j++){
+            data[i*3+j] = i;
+        }
     }
     fwrite(&data, sizeof(data), 1, outfp);
     fclose(outfp);
