d009eededd4d94dc5c7379c2e1cbe28295e7b1c3
fumi <fumi01200924@gmail.com>
2021-12-18 22:08:02 +0900

    1280*720対応のためdebug有効にできるようにした(見づらい)

--- raw_macroblock.c
+++ raw_macroblock.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-#define HEIGHT  96
-#define WIDTH   96
+//#define debug
+
+#define HEIGHT  720//96
+#define WIDTH   1280//96
 #define DEPTH   3
-static const unsigned char inputname[] = "duchshund_square.raw";
-static const unsigned char outputname[] = "output_macroblock.txt";
+static const unsigned char inputname[] = "duchshund_16_9.raw";
+static const unsigned char outputname[] = "output_duchshund16_9.txt";
 
 int main(void){
     FILE *fp = NULL;
@@ -25,14 +27,16 @@ int main(void){
         //マクロブロックの中身
     for(int block_y=0; block_y<HEIGHT/8; block_y++){
         for(int block_x=0; block_x<WIDTH/8; block_x++){
+#ifdef debug
             printf("%d block_x, %d block_y\n",block_x, block_y);
+#endif
             for(int y=0; y<8; y++){
                 for(int x=0; x<8; x++){
                     int addr = ((x+block_x*8) + (y*WIDTH + HEIGHT*8*block_y)) * DEPTH;
-
                     fprintf(outfp, "%02x", mem[addr + 2]);
                     fprintf(outfp, "%02x", mem[addr + 1]);
                     fprintf(outfp, "%02x", mem[addr + 0]);
+#ifdef debug
                     printf("%02x", mem[addr + 2]);
                     printf("%02x", mem[addr + 1]);
                     printf("%02x", mem[addr + 0]);
@@ -41,13 +45,20 @@ int main(void){
                     //     printf("%02x", mem[addr+i]);
                     // }
                     printf(" ");
+#endif
                     fprintf(outfp, "\n");
                 }
+#ifdef debug
                 printf("\n");
+#endif
             }
+#ifdef debug
             printf("\n");
+#endif
         }
+#ifdef debug
         printf("\n");
+#endif
     }
     printf("\ndone.\n");
 
