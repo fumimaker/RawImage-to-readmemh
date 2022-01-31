076a68dd0644e2098f6a5c44d03640bf5a20e716
fumi <fumi01200924@gmail.com>
2022-01-31 16:02:22 +0900

    くっつける太郎ができた

--- makejpeg.c
+++ makejpeg.c
@@ -3,12 +3,15 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <string.h>
 // #define debug
 //////////////////////////////////////////////////////////
 // エンコードbitstreamをヘッダと合成
 //////////////////////////////////////////////////////////
 static const unsigned char inputname[] = "input/duchshund720.raw";
 static const unsigned char outputname[] = "output/duchshund720_readmemh.txt";
+
+static const unsigned char headername[]="headerout.bin";
 
 // int GetFileSize(const char* FileName[]) {
 //     int fsize = 0;
@@ -21,49 +24,64 @@ static const unsigned char outputname[] = "output/duchshund720_readmemh.txt";
 // }
 
 int main(void) {
-    unsigned char mem[50*1024];
-    FILE *fp;
-    char moji[32];
+    unsigned char mem[50*1024], header[1024], buf[50*1024];
+    unsigned char eof[2]={0xFF, 0x09};
 
-    // for(int i=0; i<90; i++){
-    //     sprintf(moji, "%d.bin", i);
-    //     fp[i] = fopen(moji, "rb");
-    //     if (fp == NULL) {
-    //         printf("%d no file.\n",i);
-    //         return -1;
-    //     }
-    //     int filesize = GetFileSize(moji);
-    //     fread(mem[i], sizeof(unsigned char), filesize, fp[i]);
-    // }
+    FILE *fp, *fp_header;
+    char moji[32];
+    struct stat sb;
+    int sizeofheader, sizeofdata;
+
+    fp_header = fopen(headername, "rb");
+    if (fp_header == NULL) {
+        printf("header no file.\n");
+        return -1;
+    }
+    if (stat(headername, &sb) == -1) {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+    sizeofheader = sb.st_size;
+    fread(header, sizeof(unsigned char), sizeofheader, fp_header);
 
     int i = 0;
-    sprintf(moji, "%02d.bin", i);
+    sprintf(moji, "bitstream/%02d.bin", i);
     fp = fopen(moji, "rb");
     if (fp == NULL) {
-        printf("%d no file.\n", i);
+        printf("%02d no file.\n", i);
         return -1;
     }
 
-    struct stat sb;
+    
     if (stat(moji, &sb) == -1) {
         perror("stat");
         exit(EXIT_FAILURE);
     }
-    printf("filesize:%d\n", (int)sb.st_size);
-    fread(mem, sizeof(unsigned char), sb.st_size, fp);
+    sizeofdata = sb.st_size;
 
-    for(int k=0; k<sb.st_size; k++){
-        printf("%02x ", mem[k]);
-        if((k+1)%0xF==0){
+    fread(mem, sizeof(unsigned char), sizeofdata, fp);
+
+    printf("\n");
+
+    memcpy(buf, header, sizeofheader);
+    for (int k = 0; k < sizeofheader; k++) {
+        printf("%02x ", buf[k]);
+        if ((k + 1) % 0xF == 0) {
             printf("\n");
         }
     }
-    
+    printf("\n\n");
 
+    memcpy(buf + sizeofheader, mem, sizeofdata);
+    memcpy(buf + sizeofheader + sizeofdata, eof, 2);
+    FILE *fp_out = fopen("jpeg/00.jpg","wb");
+    fwrite(buf, sizeof(unsigned char), sizeofheader + sizeofdata + 2, fp_out);
+
+    printf("sizeofheader:%d bytes\n", sizeofheader);
+    printf("filesize:%d bytes\n", sizeofdata);
     printf("\ndone.\n");
     fclose(fp);
-    for(int j=0; j<90; j++){
-        
-    }
+    fclose(fp_header);
+    fclose(fp_out);
     return 0;
 }
