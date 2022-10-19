2ef4c9f97ddd642f5ec31b237aaaf08db9cc3427
fumi <fumi01200924@gmail.com>
2022-10-20 01:24:24 +0900

    テキストで読んでそのままXY座標ベースの配列に突っ込むようにしたがやっぱり変化はなかった．

--- decoder/vivado_text.c
+++ decoder/vivado_text.c
@@ -5,7 +5,7 @@
 #define DEPTH   3
 
 static const unsigned char inputname[] = "outputraw.raw";  //"renzoku.txt";
-static const unsigned char outputname[] = "output_reordering.raw";
+static const unsigned char outputname[] = "output_text_to_raw.raw";
 
 int main(void) {
 	FILE *fp = NULL;
@@ -25,9 +25,19 @@ int main(void) {
 
     int x=0, y=0;
     unsigned char r=0, g=0, b=0;
+    unsigned int image[HEIGHT][WIDTH];
     while (fscanf(fp, "%04x %04x %02hhx %02hhx %02hhx", &x, &y, &r, &g, &b)!=EOF){
-        printf("%04x %04x %02x %02x %02x\n", x, y, r, g, b);
+        image[y][x] = (r << 16) | (g << 8) | (b << 0);
+        printf("%06x %04x %04x %02x %02x %02x\n", image[y][x], x, y, r, g, b);
+
     }
-        fclose(fp);
-        fclose(outfp);
+    for(int i=0; i<HEIGHT; i++){
+        for(int j=0; j<WIDTH; j++){
+            fprintf(outfp, "%c", (image[i][j] & 0xFF0000) >> 16);
+            fprintf(outfp, "%c", (image[i][j] & 0x00FF00) >> 8);
+            fprintf(outfp, "%c", (image[i][j] & 0x0000FF) >> 0);
+        }
+    }
+    fclose(fp);
+    fclose(outfp);
 }
