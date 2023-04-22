15fe9c71ad807e826b93f48fe6048a4b910d689f
fumi <fumi@sfc.wide.ad.jp>
2023-04-23 00:19:15 +0900

    add parameter.c

---
+++ parameters.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void) {
+	for (int y=1; y<=8; y++){
+		for(int x=1; x<=8; x++){
+			printf("parameter QQ%d_%d	= (Q%d_%d==1)	? (4096/Q%d_%d) : (4096/Q%d_%d)+1;\n", y,x,y,x,y,x,y,x);
+		}
+	}
+}
