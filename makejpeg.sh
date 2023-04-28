09d4ee7051042277d67e74d0b2f159c37a5c486d
fumi <fumi@sfc.wide.ad.jp>
2023-04-28 20:29:46 +0900

    changed IP addr

--- makejpeg.sh
+++ makejpeg.sh
@@ -1,9 +1,10 @@
 # DIR = "dma_udp"
 # DIR = "rcvudp"
+host=203.178.128.111
 
 rm jpeg/*
 rm binout/*
 # scp root@203.178.128.231:~/ultra96_software/dma_udp/binout/* ./binout/ #Ultra96自身のデータをjpegにしたいとき
 # scp fumi@MacBooK-Prokamoshirenai.local:~/GitHub/ultra96_software/rcvudp/binout/* ./binout/ # Macのデータをjpegにしたいとき
-scp fumi@203.178.128.70:/home/fumi/github/receivejpeg/rcvudp/binout/* ./binout/
+scp fumi@${host}:/home/fumi/github/receivejpeg/rcvudp/binout/* ./binout/
 ./makejpeg # VM CUIのデータをjpegにしたいとき
